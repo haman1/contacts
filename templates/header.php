9df662a75b77beb45d8b076d36fcae1f956e36dc
ombasa <ombasaherman@gmail.com>
2022-01-30 17:31:54 +0300

    header template

---
+++ templates/header.php
@@ -0,0 +1,22 @@
+<head>
+	<title>Contact Manager</title>
+	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+	  .brand{
+	  	background: #B62E11 !important;
+	  }
+  	.brand-text{
+  		color: #B62E11 !important;
+  	}
+  </style>
+</head>
+<body class="grey lighten-4">
+	<nav>
+    <div class="container">
+      <a href="#" class="brand-logo brand-text">Contacts Manager</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li><a href="#" class="btn brand z-depth-0">Add a Contact</a></li>
+      </ul>
+    </div>
+  </nav>
